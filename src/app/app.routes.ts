cbd3cd442002065ba81efdbbda79a98bd43cfafa
Ronaldo Torres <rtorres66@misena.edu.co>
2024-11-13 08:44:37 -0500

    add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        loadComponent: () => import('./web/home/home.component'),
+        children: [
+            {
+                path: 'about',
+                loadComponent: () => import('./web/about/about.component')
+            }
+        ]
+    }
+
+];
