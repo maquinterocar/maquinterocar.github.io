5a4808914e37c7179a4d2ae76fa9bb7f0806d2a7
Ronaldo Torres <rtorres66@misena.edu.co>
2024-11-26 20:31:54 -0500

    Landing page init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,10 +6,6 @@ export const routes: Routes = [
         path: '',
         loadComponent: () => import('./web/home/home.component'),
         children: [
-            {
-                path: 'about',
-                loadComponent: () => import('./web/about/about.component')
-            }
         ]
     }
 
--- src/app/web/home/home.component.ts
+++ src/app/web/home/home.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './home.component.html',
 })
 export default class HomeComponent {
+
+  year = signal(new Date().getFullYear());
+
+  private fb = inject(FormBuilder);
+
+  get name() {
+    return this.formJoin.get('name')?.invalid && this.formJoin.get('name')?.touched;
+  }
+  get email() {
+    return this.formJoin.get('email')?.invalid && this.formJoin.get('email')?.touched;
+  }
+
+
+  formJoin = this.fb.group({
+    name: [, Validators.required],
+    email: [, [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+    message: [, Validators.required],
+  });
+
+
+  send() {
+    if (this.formJoin.invalid) {
+      alert('Debe completar todos los campos');
+    }
+    console.log(this.formJoin.value);
+  }
 
 }
